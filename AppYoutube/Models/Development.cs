b6c0039af1e50ae9114bd1b821d7f8fe0019e736
Wender Dalber <wenderdalber@gmail.com>
2020-03-05 19:02:21 -0300

    Inicio do projeto, criacao do metodo de pesquisa, instalacao da biblioteca Youtube.APi.v3 para aspnetcore21

---
+++ AppYoutube/Models/Development.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Upload;
+using Google.Apis.Util.Store;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+
+namespace AppYoutube.Models
+{
+    public class Development
+    {
+        public List<string> DadosYoutTube(string word)
+        {
+            try
+            {
+                return Run(word);
+            }
+            catch (AggregateException ex)
+            {
+                foreach (var e in ex.InnerExceptions)
+                {
+                    Console.WriteLine("Error: " + e.Message);
+                }
+                return new List<string>();
+            }
+        }
+
+        /// <summary>
+        /// Através da API do google que se encontra disponível no nugget
+        /// realiza uma conexão e busca um conteudo no youtube de acordo com
+        /// o que for digitado
+        /// </summary>
+        /// <param name="word"></param>
+        /// <returns></returns>
+        public List<string> Run(string word)
+        {
+            var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = "AIzaSyDoKfEuXhDFElw3qL3INlvA7KKfOwp9h6A",
+                ApplicationName = this.GetType().ToString()
+            });
+
+            var searchListRequest = youtubeService.Search.List("snippet");
+            searchListRequest.Q = word; // Replace with your search term.
+            searchListRequest.MaxResults = 50;
+
+            // Call the search.list method to retrieve results matching the specified query term.
+            //var searchListResponse = await searchListRequest.ExecuteAsync();
+            var searchListResponse = searchListRequest.Execute();
+
+            List<string> videos = new List<string>();
+            List<string> channels = new List<string>();
+            List<string> playlists = new List<string>();
+
+            // Add each result to the appropriate list, and then display the lists of
+            // matching videos, channels, and playlists.
+            foreach (var searchResult in searchListResponse.Items)
+            {
+                switch (searchResult.Id.Kind)
+                {
+                    case "youtube#video":
+                        videos.Add(String.Format("{0} ({1})", searchResult.Snippet.Title, searchResult.Id.VideoId));
+                        break;
+
+                    case "youtube#channel":
+                        channels.Add(String.Format("{0} ({1})", searchResult.Snippet.Title, searchResult.Id.ChannelId));
+                        break;
+
+                    case "youtube#playlist":
+                        playlists.Add(String.Format("{0} ({1})", searchResult.Snippet.Title, searchResult.Id.PlaylistId));
+                        break;
+                }
+            }
+
+            //Console.WriteLine(String.Format("Videos:\n{0}\n", string.Join("\n", videos)));
+            //Console.WriteLine(String.Format("Channels:\n{0}\n", string.Join("\n", channels)));
+            //Console.WriteLine(String.Format("Playlists:\n{0}\n", string.Join("\n", playlists)));
+            return channels;
+        }
+    }
+}
