0465fb321cac0325aed77f1a73196ede6df03acd
Wender Dalber <wenderdalber@gmail.com>
2020-03-05 21:29:06 -0300

    configuracao do ambiente mongodb com o objeto pesquisa, classificacao e classe de conexao

---
+++ AppYoutube/Models/Dados.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppYoutube.Models
+{
+    public class Dados
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Name")]
+        public string Name { get; set; }
+
+        public string URL { get; set; }
+
+        public string Description { get; set; }
+    }
+}
