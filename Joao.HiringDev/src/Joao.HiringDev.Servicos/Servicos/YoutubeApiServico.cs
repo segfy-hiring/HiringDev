74f112058f20e342b0aa31e2f155896a2395965a
joaocarias <joaocariasdefranca@gmail.com>
2021-07-21 14:53:36 -0300

    iniciando implementacao de servico do youtube api

--- Joao.HiringDev/src/Joao.HiringDev.Servicos/Servicos/YoutubeApiServico.cs
+++ Joao.HiringDev/src/Joao.HiringDev.Servicos/Servicos/YoutubeApiServico.cs
@@ -1,9 +1,26 @@
-﻿using Joao.HiringDev.Servicos.Core.IServicos;
+﻿using Google.Apis.Samples.Helper;
+using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+
+using Joao.HiringDev.Servicos.Core.IServicos;
 
 namespace Joao.HiringDev.Servicos.Servicos
 {
     public class YoutubeApiServico : IYoutubeApiServico
     {
+        public void Pesquisar()
+        {
+            YoutubeService youtube = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = ""
+            });
 
+            SearchResource.ListRequest listRequest = youtube.Search.List("snippet");
+            listRequest.Q = CommandLine.RequestUserInput<string>("Search term: ");
+            listRequest.Order = SearchResource.Order.Relevance;
+
+            SearchListResponse searchResponse = listRequest.Fetch();
+        }
     }
 }
