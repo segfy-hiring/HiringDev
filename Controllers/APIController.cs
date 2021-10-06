0735754a57d6059ae257f6b70d68e9b49bfeedd5
Daniel Conzo <dcruas@yahoo.com.br>
2021-10-06 18:38:01 -0300

    commit inicial

---
+++ Controllers/APIController.cs
@@ -0,0 +1,31 @@
+﻿using IutubiProject.Service;
+using IutubiRestfulAPI.Database;
+using IutubiRestfulAPI.Interfaces;
+using IutubiRestfulAPI.Service;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IutubiProject.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        public YoutubeService youtubeService { get; set; }
+
+        public ApiController()
+        {
+            youtubeService = new YoutubeService(new YoutubeItemDB());
+        }
+
+        [HttpGet("{title}")]
+        public async Task<IActionResult> Import(string title) 
+            => await youtubeService.Import(title);
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+            => await youtubeService.GetAll();
+
+    }
+}
---
+++ Database/YoutubeItemDB.cs
@@ -0,0 +1,55 @@
+﻿using IutubiRestfulAPI.Interfaces;
+using IutubiRestfulAPI.Service;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IutubiRestfulAPI.Database
+{
+    public class YoutubeItemDB : IYoutubeItemDB
+    {
+        private string connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
+        
+
+        public async Task<List<YoutubeItem>> Get()
+        {
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("Iutubi");
+            var collection = database.GetCollection<YoutubeItem>("YoutubeItems");
+            var filter = Builders<BsonDocument>.Filter.Empty;
+            var sort = Builders<BsonDocument>.Sort.Ascending("time");           
+            var list = await collection.FindAsync(new BsonDocument());
+            
+            
+            return list.ToList();
+        }
+
+        public async Task<bool> Insert(List<YoutubeItem> resultList)
+        {
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("Iutubi");
+            var collection = database.GetCollection<YoutubeItem>("YoutubeItems");
+
+            foreach (var item in resultList)
+            {
+                try
+                {
+                    if (collection.CountDocuments(x => x.YoutubeID == item.YoutubeID && x.Kind == item.Kind) == 0)
+                        await collection.InsertOneAsync(item);
+                }
+                catch (Exception)
+                {
+                    return false;
+                }                
+
+            }
+
+            return true;
+        }
+
+    }
+}
